c1d92760763260363f313204eaa6baa72016e4e0
Alexander Shatalov <alexander.shataloff@gmail.com>
2019-11-17 10:14:26 +0300

    [ex2-95]: Упростил решение. Дополнения.

--- local/classes/Exam2/Handlers/ContentMenuEx2Task95.php
+++ local/classes/Exam2/Handlers/ContentMenuEx2Task95.php
@@ -18,21 +18,25 @@ class ContentMenuEx2Task95
     {
         global $USER;
         // Если пользователь принадлежит группе "Контент-редакторы" (5) и не принадлежит группе "Администраторы" (1)
-        // тогда, убираем пункты меню
+        // тогда, изменяем структуру меню
         if (in_array(5, $USER->GetUserGroupArray()) && !in_array(1, $USER->GetUserGroupArray())) {
             // Пункт меню "Рабочий стол"
             unset($aGlobalMenu['global_menu_desktop']);
+
             // Убираем подпункты, если есть следующие родительские пункты меню
             foreach ($aModuleMenu as $iKey => $sValue) {
-                // Можно удалить лишние пункты
-                // Либо оставить только нужный
-                // + "parent_menu" => "global_menu_content"
-                // + "items_id" => "menu_iblock_/news"
+                /* Можно удалить лишние пункты
                 if ($aModuleMenu[$iKey]['parent_menu'] == 'global_menu_settings' // Настройки
                     || $aModuleMenu[$iKey]['parent_menu'] == 'global_menu_services' // Сервисы
                     || $aModuleMenu[$iKey]['parent_menu'] == 'global_menu_marketplace' // Маркетплейс
                     || $aModuleMenu[$iKey]['items_id'] == 'menu_iblock' // Инфоблоки (Импорт, экспорт и т.п.)
                 ) {
+                    unset($aModuleMenu[$iKey]);
+                }
+                */
+
+                // Либо оставить только нужный
+                if ($aModuleMenu[$iKey]['items_id'] != 'menu_iblock_/news') {
                     unset($aModuleMenu[$iKey]);
                 }
             }
